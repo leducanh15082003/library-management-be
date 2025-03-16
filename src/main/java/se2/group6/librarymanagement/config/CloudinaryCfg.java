0be24d70c917728c60d3868b177e050f8447bb65
leducanh1508 <bokholeki@gmail.com>
2025-03-16 16:35:59 +0700

    secure

--- src/main/java/se2/group6/librarymanagement/config/CloudinaryCfg.java
+++ src/main/java/se2/group6/librarymanagement/config/CloudinaryCfg.java
@@ -2,17 +2,28 @@ package se2.group6.librarymanagement.config;
 
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CloudinaryCfg {
+
+    @Value("${cloudinary.cloud_name}")
+    private String cloudName;
+
+    @Value("${cloudinary.api_key}")
+    private String apiKey;
+
+    @Value("${cloudinary.api_secret}")
+    private String apiSecret;
+
     @Bean
     public Cloudinary cloudinary() {
         return new Cloudinary(ObjectUtils.asMap(
-                "cloud_name", "dlqpdl4mz",
-                "api_key", "461146261413479",
-                "api_secret", "YJ03eeKoRH21wUx3dyPStyUK4pw",
+                "cloud_name", cloudName,
+                "api_key", apiKey,
+                "api_secret", apiSecret,
                 "secure", true
         ));
     }
