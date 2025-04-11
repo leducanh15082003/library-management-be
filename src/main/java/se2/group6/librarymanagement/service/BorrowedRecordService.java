5d30a53ed0f6266f7e8a4fa5bb530592957ffbbc
Your Name <youremail@example.com>
2025-04-11 14:50:03 +0700

    trang mượn trả

--- src/main/java/se2/group6/librarymanagement/service/BorrowedRecordService.java
+++ src/main/java/se2/group6/librarymanagement/service/BorrowedRecordService.java
@@ -5,8 +5,17 @@ import se2.group6.librarymanagement.model.BorrowedRecord;
 import java.util.List;
 
 public interface BorrowedRecordService {
+
     void saveBorrowedRecord(BorrowedRecord borrowedRecord);
+
     List<BorrowedRecord> findByUserId(Long id);
+
     List<BorrowedRecord> findByReturnAtIsNull();
+
+    List<BorrowedRecord> findAll();
+
+    void markAsReturned(Long id);
+
+    List<BorrowedRecord> findByStudentId(String studentId); // Thêm phương thức tìm kiếm theo studentId
 
 }
